ce147e096cc9af5e7570511239f34ec7806e1d99
bpavlicTMF <borna.pavlic@yahoo.com>
2024-03-08 16:29:00 +0100

    feat: changes fromEmail

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,11 +4,13 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
+const fromEmail = 'mail@next14-auth-v5.com';
+
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: 'no-reply@next14-auth-v5.com',
+    from: fromEmail,
     to: email,
     subject: 'Please verify your email',
     html: `
@@ -22,7 +24,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'no-reply@next14-auth-v5.com',
+    from: fromEmail,
     to: email,
     subject: 'Reset your password',
     html: `
@@ -34,7 +36,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: 'no-reply@next14-auth-v5.com',
+    from: fromEmail,
     to: email,
     subject: '2FA code',
     html: `<p>Your 2FA code: ${token}</p>`,
