e342e57ff4cc1693a5dda631126d51716870eb9d
bpavlicTMF <borna.pavlic@yahoo.com>
2024-03-08 16:17:08 +0100

    feat: adds a new email

--- lib/mail.ts
+++ lib/mail.ts
@@ -8,7 +8,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'no-reply@next14-auth-v5.com',
     to: email,
     subject: 'Please verify your email',
     html: `
@@ -22,7 +22,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'no-reply@next14-auth-v5.com',
     to: email,
     subject: 'Reset your password',
     html: `
@@ -34,7 +34,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'no-reply@next14-auth-v5.com',
     to: email,
     subject: '2FA code',
     html: `<p>Your 2FA code: ${token}</p>`,
